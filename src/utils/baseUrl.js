594f3efdc56b62f45a0acaa1fa36c216816ccf5c
hiepdeptrai0908 <hiepdeptrai0908@gmail.com>
2022-12-10 08:16:02 +0900

    update

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:8080/api/'
+const baseURL = 'https://staff-333-api.herokuapp.com/api/'
 // const baseURL = 'https://db9b-124-144-119-75.jp.ngrok.io/'
 
 export default baseURL
