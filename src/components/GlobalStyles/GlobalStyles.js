f6e22b595a36cbf08da885687e1819f3d5b6c3a1
hiepdeptrai0908 <hiepdeptrai0908@gmail.com>
2023-01-14 23:23:12 +0900

    ud

--- src/components/GlobalStyles/GlobalStyles.js
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -1,6 +1,11 @@
 import './GlobalStyles.scss'
+import images from '~/assets/images'
 
 function GlobalStyles({ children }) {
+    const backgroundLink = localStorage.getItem('bg') || 'goldBg'
+
+    const htmlEl = document.getElementsByTagName('html')[0]
+    htmlEl.style.backgroundImage = `url('${images[backgroundLink]}')`
     return children
 }
 
