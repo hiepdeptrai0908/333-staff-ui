595cc4e14f4adeaeffc54a41fdc19f8b29570af6
hiepdeptrai0908 <hiepdeptrai0908@gmail.com>
2025-11-13 15:49:53 +0900

    change backend

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,5 @@
-const baseURL = 'https://staff-333-api.herokuapp.com/api/'
+const baseURL = 'http://tamabee.vn:8888/api/'
+// const baseURL = 'https://staff-333-api.herokuapp.com/api/'
 // const baseURL = 'http://localhost:8080/api/'
 
 export default baseURL
